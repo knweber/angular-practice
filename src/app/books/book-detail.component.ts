ba8451d8db49b17c42d977fad16c65960c4a9156
Kris Weber <kris.weber@slalom.com>
2018-03-01 16:48:40 -0800

    Add to book detail functionality

--- src/app/books/book-detail.component.ts
+++ src/app/books/book-detail.component.ts
@@ -1,17 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 import { IBook } from './book';
+import { BookService } from './book.service';
 
 @Component({
   templateUrl: './book-detail.component.html',
   styleUrls: ['./book-detail.component.css']
 })
+
 export class BookDetailComponent implements OnInit {
-  pageTitle: string = 'Book Detail';
+  pageTitle = 'Book Detail';
+  errorMessage: string;
   book: IBook;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router,
+              private _bookService: BookService) { }
 
   ngOnInit() {
+    const param = +this._route.snapshot.paramMap.get('id');
+    if (param) {
+      const id = +param;
+      this.getBook(id);
+    }
+  }
+
+  getBook(id: number) {
+    this._bookService.getBook(id).subscribe(
+      book => this.book = book,
+      error => this.errorMessage = <any>error);
+  }
+
+  onBack(): void {
+    this._router.navigate(['/books']);
   }
 
 }
--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/throw';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 
 // We are not using this service to share data (as it has no properties assigned to it), but rather to encapsulate the data access features --> we are moving the responsibility for managing the data away from the component
 @Injectable()
@@ -15,6 +16,10 @@ export class BookService {
 
   getBooks(): Observable<IBook[]> {
     return this._http.get<IBook[]>(this._bookUrl).do(data => console.log('All: ' + JSON.stringify(data))).catch(this.handleError);
+  }
+
+  getBook(id: number): Observable<IBook[]> {
+    return this.getBooks().map((books: IBook[]) => books.find(b => b.bookId === id));
   }
 
   private handleError(err: HttpErrorResponse) {
